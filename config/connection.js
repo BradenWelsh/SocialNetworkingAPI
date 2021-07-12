78134ba0bb40e8e811dd8fc5e31a4ab2b277f479
Braden Welsh <BradenWelsh@users.noreply.github.com>
2021-07-12 17:33:52 -0500

    added Database and User.js

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,7 @@ let sequelize;
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-  Sequelize = new Sequelize(
+  sequelize = new Sequelize(
     process.env.DBNAME,
     process.env.DBUSER,
     process.env.DBPW,
